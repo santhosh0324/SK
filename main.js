c8be58df4f0ea9bcc9bb97cd6259cc62fc8576fd
santhosh0324 <85010102+santhosh0324@users.noreply.github.com>
2021-06-10 15:15:38 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-  if (window.scrollY > 150) {
-    document.querySelector('#nav').style.opactiy=0.9;
-  } else {
-    document.querySelector('#nav')
- .style.opactiy= 1; }
-}); 
